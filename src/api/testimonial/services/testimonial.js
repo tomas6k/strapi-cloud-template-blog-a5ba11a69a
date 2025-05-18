f296d4ef1b2cf6532b4765f9cdc9d2b5b507581c
Tomas Cikanauskas <tomas.cikanauskas@proton.me>
2025-05-18 19:02:42 +0200

    ajout testimonial

--- src/api/testimonial/services/testimonial.js
+++ src/api/testimonial/services/testimonial.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * article service.
+ * testimonial service
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::article.article');
+module.exports = createCoreService('api::testimonial.testimonial');
